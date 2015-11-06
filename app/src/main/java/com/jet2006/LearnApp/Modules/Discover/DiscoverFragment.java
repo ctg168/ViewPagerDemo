034af5c92e39f8424fbe44858aa819a0b9cb8133
terry <ctg168@gmail.com>
2015-11-06 14:44:59 +0800

    azz

--- app/src/main/java/com/jet2006/LearnApp/Modules/Discover/DiscoverFragment.java
+++ app/src/main/java/com/jet2006/LearnApp/Modules/Discover/DiscoverFragment.java
@@ -1,10 +1,12 @@
 package com.jet2006.LearnApp.Modules.Discover;
 
 import android.content.Intent;
+import android.graphics.Typeface;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import com.jet2006.LearnApp.BaseFragment;
 import com.jet2006.LearnApp.R;
@@ -14,11 +16,15 @@ public class DiscoverFragment extends BaseFragment {
 
     View itemNof;
 
+    TextView tvTitle222;
+
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View v = inflater.inflate(R.layout.fragment_discover, container, false);
+
+        tvTitle222 = (TextView) v.findViewById(R.id.txtTitle);
 
         itemNof = v.findViewById(R.id.leftBtnMyInfo);
         itemNof.setOnClickListener(new View.OnClickListener() {
@@ -29,8 +35,17 @@ public class DiscoverFragment extends BaseFragment {
             }
         });
 
+        Typeface iconfont = Typeface.createFromAsset(getActivity().getAssets(),"material_icons.ttf");
+        tvTitle222.setTypeface(iconfont);
+
         return v;
     }
 
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
 
+
+
+    }
 }
